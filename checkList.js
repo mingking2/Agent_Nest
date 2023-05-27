da6ad70e978199165cb585672307d701f1f49ffe
King's meow <mingee641@gmail.com>
2023-05-27 14:51:36 +0900

    js 기능별 분리 및 화살표함수로 전환

---
+++ checkList.js
@@ -0,0 +1,80 @@
+const users = [
+    { name: '김민경', sex: 'female', checked: false },
+    { name: '진영주', sex: 'female', checked: false },
+    { name: '이성민', sex: 'female', checked: false },
+    { name: '안혜주', sex: 'female', checked: false },
+    { name: '이진환', sex: 'male', checked: false },
+    { name: '피재호', sex: 'male', checked: false },
+    { name: '박순필', sex: 'male', checked: false },
+    { name: '안영재', sex: 'male', checked: false },
+    { name: '박세형', sex: 'male', checked: false },
+    { name: '이동우', sex: 'male', checked: false },
+    { name: '김준영', sex: 'male', checked: false },
+    { name: '정만수', sex: 'male', checked: false },
+    { name: '신영진', sex: 'male', checked: false },
+    { name: '김성욱', sex: 'male', checked: false },
+    { name: '전동엽', sex: 'male', checked: false }
+];
+
+const resetBtn = document.getElementById("reset-btn");
+
+let countNum = 0;
+
+const init = () => renderCheckboxes(); 
+
+const renderCheckboxes = () => {
+    const checkboxDiv = document.getElementById("checkboxes");
+    checkboxDiv.innerHTML = "";
+    let user_num = 1;
+    const checkNum = document.getElementById("checknum");
+    checkNum.innerText = `체크된 인원: ${countNum} 명`;
+
+    users.forEach((user) => {
+        const userElement = document.createElement('li');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = `user_${user_num}`;
+        checkbox.checked = user.checked;
+        checkbox.addEventListener('click', () => checkNumer(user.name));
+
+        const label = document.createElement('label');
+        label.setAttribute('for', `user_${user_num++}`);
+        label.textContent = user.name;
+
+        userElement.appendChild(checkbox);
+        userElement.appendChild(label);
+        checkboxDiv.appendChild(userElement);
+
+    });
+
+}
+
+const checkNumer = (name_id) => {
+    const user_name = users.find(user_name => user_name.name === name_id);
+    // user_name.name === name_id 인 조건을 찾아 조건에 해당하는 name을 반환한다.
+    if (user_name) {
+        user_name.checked = !user_name.checked;
+        // 체크박스를 눌렀을 때 정보는 false이기 때문에 역으로 만들어줌
+        if (user_name.checked) {
+            countNum++;
+        } else {
+            countNum--;
+        }
+    }
+
+    const checkNum = document.getElementById("checknum");
+    checkNum.innerText = `체크된 인원: ${countNum} 명`;
+}
+
+resetBtn.addEventListener("click", () => {
+    users.forEach((user) => {
+        user.checked = false;
+    });
+    countNum = 0;
+    const checkNum = document.getElementById("checknum");
+    checkNum.innerText = `체크된 인원: ${countNum} 명`;
+    renderCheckboxes();
+});
+
+init();
--- ladderGame.js
+++ ladderGame.js
@@ -1,95 +1,14 @@
-const users = [
-    { name: '김민경', sex: 'female', checked: false },
-    { name: '진영주', sex: 'female', checked: false },
-    { name: '이성민', sex: 'female', checked: false },
-    { name: '안혜주', sex: 'female', checked: false },
-    { name: '이진환', sex: 'male', checked: false },
-    { name: '피재호', sex: 'male', checked: false },
-    { name: '박순필', sex: 'male', checked: false },
-    { name: '안영재', sex: 'male', checked: false },
-    { name: '박세형', sex: 'male', checked: false },
-    { name: '이동우', sex: 'male', checked: false },
-    { name: '김준영', sex: 'male', checked: false },
-    { name: '정만수', sex: 'male', checked: false },
-    { name: '신영진', sex: 'male', checked: false },
-    { name: '김성욱', sex: 'male', checked: false },
-    { name: '전동엽', sex: 'male', checked: false }
-];
-
-const resetBtn = document.getElementById("reset-btn");
-
-let countNum = 0;
-
-
-init();
-
-function init() {
-    renderCheckboxes();
-}
-
-function renderCheckboxes() {
-    const checkboxDiv = document.getElementById("checkboxes");
-    checkboxDiv.innerHTML = "";
-    let user_num = 1;
-    const checkNum = document.getElementById("checknum");
-    checkNum.innerText = `체크된 인원: ${countNum} 명`;
-
-    users.forEach((user) => {
-        const userElement = document.createElement('li');
-
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = `user_${user_num}`;
-        checkbox.checked = user.checked;
-        checkbox.addEventListener('click', () => checkNumer(user.name));
-
-        const label = document.createElement('label');
-        label.setAttribute('for', `user_${user_num++}`);
-        label.textContent = user.name;
-
-        userElement.appendChild(checkbox);
-        userElement.appendChild(label);
-        checkboxDiv.appendChild(userElement);
-
-    });
-
-}
-
-function checkNumer(name_id) {
-    const user_name = users.find(user_name => user_name.name === name_id);
-    // user_name.name === name_id 인 조건을 찾아 조건에 해당하는 name을 반환한다.
-    if (user_name) {
-        user_name.checked = !user_name.checked;
-        // 체크박스를 눌렀을 때 정보는 false이기 때문에 역으로 만들어줌
-        if (user_name.checked) {
-            countNum++;
-        } else {
-            countNum--;
-        }
-    }
-
-    const checkNum = document.getElementById("checknum");
-    checkNum.innerText = `체크된 인원: ${countNum} 명`;
-}
-
-resetBtn.addEventListener("click", () => {
-    users.forEach((user) => {
-        user.checked = false;
-    });
-    countNum = 0;
-    const checkNum = document.getElementById("checknum");
-    checkNum.innerText = `체크된 인원: ${countNum} 명`;
-    renderCheckboxes();
-});
-
-
 const numberBtn = document.getElementById('numOfplayers');
 const settingBtn = document.getElementById('setting');
 let numOfplayers = 0;
 let ladder_arr;
 let rows, cols;
 
-function settingPlayer() {
+settingBtn.addEventListener('click', () => {
+    settingPlayer();
+})
+
+const settingPlayer = () => {
     numOfplayers = document.getElementById('numOfplayers').value;
     if (numOfplayers < 3 || numOfplayers > 100) {
         alert("플레이어의 수가 에바임");
@@ -104,7 +23,7 @@ function settingPlayer() {
     input_player.innerHTML = "";
     input_dest.innerHTML = "";
 
-    for (let i = 1; i <= numOfplayers; i++) {
+    Array.from({ length: numOfplayers }, (_,i) => {
         const input = document.createElement("input");
         input.setAttribute("type", "text");
         input.setAttribute("id", `player-${i}`);
@@ -117,11 +36,11 @@ function settingPlayer() {
 
         input_player.appendChild(input);
         input_dest.appendChild(destInput);
-    }
+    });
 
 }
 
-function createLadder() {
+const createLadder = () => {
     const playerNames = [];
     const destinations = [];
     let index_play = 0;
@@ -164,6 +83,7 @@ function createLadder() {
             }
         }
     }
+
 
     const random_ladder = Math.floor(cols / 2);
     for (let i = 1; i < rows - 1; i++) {
@@ -210,7 +130,7 @@ function createLadder() {
 
 }
 
-function resetLadder() {
+const resetLadder = () => {
     const ladder = document.getElementById("ladder");
     const input_player = document.getElementById("input_players");
     const input_dest = document.getElementById('input_destinations');
@@ -230,7 +150,7 @@ function resetLadder() {
     result.style.display = "none";
 }
 
-function resultLadder() {
+const resultLadder = () => {
     let cur_i = 0;
     let i_col = 0;
 
@@ -240,7 +160,6 @@ function resultLadder() {
     next_result.style.display = 'flex';
     result.innerHTML = "";
     
-
     const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
       
     const runLoop = async () => {
