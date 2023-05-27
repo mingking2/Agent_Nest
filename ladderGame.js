73331f9379bc74d5a1bb9bb992a0579fbab38343
King's meow <mingee641@gmail.com>
2023-05-27 16:04:33 +0900

    ladder 구현부분과 기능부분을 js파일로 개별로 분리

--- ladderGame.js
+++ ladderGame.js
@@ -1,12 +1,15 @@
 const numberBtn = document.getElementById('numOfplayers');
 const settingBtn = document.getElementById('setting');
+const startBtn = document.getElementById('start');
+
+
 let numOfplayers = 0;
 let ladder_arr;
-let rows, cols;
+export let rows, cols;
 
-settingBtn.addEventListener('click', () => {
-    settingPlayer();
-})
+settingBtn.addEventListener('click', () => settingPlayer())
+startBtn.addEventListener('click', () => createLadder())
+
 
 const settingPlayer = () => {
     numOfplayers = document.getElementById('numOfplayers').value;
@@ -26,13 +29,13 @@ const settingPlayer = () => {
     Array.from({ length: numOfplayers }, (_,i) => {
         const input = document.createElement("input");
         input.setAttribute("type", "text");
-        input.setAttribute("id", `player-${i}`);
-        input.setAttribute("placeholder", `player-${i}`);
+        input.setAttribute("id", `player-${i+1}`);
+        input.setAttribute("placeholder", `player-${i+1}`);
 
         const destInput = document.createElement("input");
         destInput.setAttribute("type", "text");
-        destInput.setAttribute("id", `destination-${i}`);
-        destInput.setAttribute("placeholder", `destination-${i}`);
+        destInput.setAttribute("id", `destination-${i+1}`);
+        destInput.setAttribute("placeholder", `destination-${i+1}`);
 
         input_player.appendChild(input);
         input_dest.appendChild(destInput);
@@ -130,92 +133,5 @@ const createLadder = () => {
 
 }
 
-const resetLadder = () => {
-    const ladder = document.getElementById("ladder");
-    const input_player = document.getElementById("input_players");
-    const input_dest = document.getElementById('input_destinations');
-    const numOfplayers = document.getElementById('numOfplayers');
-    const result = document.getElementById("ladder_result");
-    const result_btn = document.getElementById('result');
-    result_btn.removeEventListener('click',resultLadder);
-
-    numOfplayers.value = "";
-    ladder.innerHTML = "";
-    input_player.innerHTML = "";
-    input_dest.innerHTML = "";
-    numberBtn.disabled = false;
-    settingBtn.disabled = false;
-    result_btn.disabled = false;
-    result.innerHTML = "";
-    result.style.display = "none";
-}
-
-const resultLadder = () => {
-    let cur_i = 0;
-    let i_col = 0;
-
-    const result = document.getElementById("ladder_result");
-    const next_result = document.getElementById("next");
-    const result_btn = document.getElementById('result');
-    next_result.style.display = 'flex';
-    result.innerHTML = "";
-    
-    const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-      
-    const runLoop = async () => {
-        console.log('cols: ' + cols);
-        if (i_col < cols) {
-          cur_i = i_col;
-          console.log(i_col);
-          console.log(cur_i);
-          for (let j = 0; j < rows; j++) {
-            console.log(document.getElementById(`${j},${cur_i}`));
-            document.getElementById(`${j},${cur_i}`).style.color = 'red';
-            await sleep(500); // 0.5초 대기
-            if (ladder_arr[j][cur_i + 1] == '───') {
-              document.getElementById(`${j},${cur_i + 1}`).style.color = 'red';
-              console.log(document.getElementById(`${j},${cur_i + 1}`));
-              await sleep(500); // 0.5초 대기
-              cur_i += 1; // 오른쪽으로 이동
-              document.getElementById(`${j},${cur_i+1}`).style.color = 'red';
-              cur_i += 1; // 오른쪽으로 이동
-              console.log(document.getElementById(`${j},${cur_i}`));
-            } else if (ladder_arr[j][cur_i - 1] == '───') {
-              console.log(document.getElementById(`${j},${cur_i - 1}`));
-              document.getElementById(`${j},${cur_i - 1}`).style.color = 'red';
-              await sleep(500); // 0.5초 대기
-              cur_i -= 1; // 왼쪽으로 이동
-              document.getElementById(`${j},${cur_i-1}`).style.color = 'red';
-              cur_i -= 1; // 왼쪽으로 이동
-              console.log(document.getElementById(`${j},${cur_i}`));
-            }
-          }
-          result.style.display = 'flex';
-          result.innerHTML += ladder_arr[0][i_col] + ' => ' + ladder_arr[rows - 1][cur_i] + '</br>';
-          i_col += 2;
-          console.log(' i_col= ' + i_col);
-        } 
-      };
-      
-
-    next_result.addEventListener('click', () => { //여기서 문제가 발생함.
-        console.log(" i_col11111= " + i_col);
-        const tdElements = document.getElementsByTagName('td');
-        for (let j = 0; j < tdElements.length; j++) {
-            tdElements[j].style.color = "black";
-        }
-        if (i_col > cols) {
-            i_col = 0;
-            console.log(" i_col= " + i_col);
-            alert('더이상 나올 데이터가 없습니다!');
-            next_result.style.display = 'none';
-            result_btn.disabled = true;
-            result_btn.removeEventListener('click', resultLadder);
-        } else {
-            runLoop();
-        }
-    });
 
 
-    runLoop(); // 버튼을 누르기 전에 첫화면 결과 보여줘야댐
-}
